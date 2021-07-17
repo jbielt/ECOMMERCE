756ff9a9f1db48234514fcbb0a0c70add0721f98
jbielt <jbielturon@gmail.com>
2021-07-17 19:54:25 +0200

    order and order-item model created

--- BACKEND/models/order.js
+++ BACKEND/models/order.js
@@ -1,7 +1,59 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
+    }],
+    shippingAddress1: {
+        type: String,
+        required: true
+    },
+    shippingAddress2: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending'
+    },
+    totalPrice: {
+        type: Number
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now
+    }
 })
+
+//crear un ID virtual que serà el utilitzat al front ( sense la _id).
+orderSchema.virtual('id').get(function (){
+    return this._id.toHexString();
+});
+orderSchema.set('toJSON',{
+    virtuals: true
+});
+
 
 exports.Order = mongoose.model('Order', orderSchema);
