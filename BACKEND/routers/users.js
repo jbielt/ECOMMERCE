a573a5bae72276eee62c2864818c554ad3f00c8f
jbielt <jbielturon@gmail.com>
2021-08-26 06:52:56 +0200

    userHash seems to be fixed. (have a look in future)

--- BACKEND/routers/users.js
+++ BACKEND/routers/users.js
@@ -44,12 +44,12 @@ router.post('/', async (req,res)=>{
         apartment: req.body.apartment,
         zip: req.body.zip,
         city: req.body.city,
-        country: req.body.country,
+        country: req.body.country
     })
     user = await user.save();
 
     if(!user)
-        return res.status(400).send('the user cannot be created!')
+        return res.status(400).send('the user cannot be created!');
 
     res.send(user);
 })
